5b2f0e9b9e90adfaed6a28cdaaa3ed732bd7e7e3
Amarnath <motorolaonefusion@gmail.com>
2022-11-24 03:39:45 +0530

    skill edit and remove option

--- src/Components/ListSkill/AllSkills.js
+++ src/Components/ListSkill/AllSkills.js
@@ -2,14 +2,42 @@ import React, { useEffect, useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid';
 import axios from '../../axios';
 import ResponsiveDialog from './Popup';
+import DepartmentPopup from './DepartmentPopup'
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #cc0e15',
+  boxShadow: 24,
+  color : "#cc0e15",
+  p: 4,
+};
 
 
 function AllSkills() {
 
 
   const [passid, setPassid] = useState('');
+
+  const naviagte = useNavigate()
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true)
+  };
+  const handleClose = () => setOpen(false);
 
   const columns = [
   { field: 'id', headerName: 'Id', width: 150 },
@@ -34,8 +62,9 @@ function AllSkills() {
       return (
         <button
         className='btn-pending'
-         onClick={(e)=>{
-          console.log(cellvalues)
+        onClick={()=>{
+          setPassid(cellvalues.row.id)
+          setOpen(true)
          }}>
           Remove
         </button>
@@ -67,11 +96,44 @@ function AllSkills() {
       }
     })
 
- 
 
+    const handleDelete=()=>{
+      axios.get(`/delete-skill/?id=${passid}`).then((res)=>{
+        setOpen(false)
+      })
+     }
+    
 
   return (
     <div className='pending-table'>
+
+
+<Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              delete id is {passid}
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Do you really want to delete the description<br/>
+              <button onClick={handleDelete} style={{border:"2px solid #cc0e15", color:"#fff" , background:"#000"}}>
+                Delete
+              </button>
+            </Typography>
+          </Box>
+        </Fade>
+  </Modal>
+
         <div style={{ height: 400, width: '100%' }}>
             <DataGrid
                 rows={rowData}
--- src/Components/ListSkill/ListCategory.js
+++ src/Components/ListSkill/ListCategory.js
@@ -3,11 +3,29 @@ import { DataGrid } from '@mui/x-data-grid';
 import { Button } from '@mui/material';
 import axios from '../../axios';
 import CategoryPopup from './CategoryPopup';
+import DepartmentPopup from './DepartmentPopup'
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
 
 
 
 
-
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #cc0e15',
+  boxShadow: 24,
+  color : "#cc0e15",
+  p: 4,
+};
 
 
 function ListCategory() {
@@ -15,6 +33,10 @@ function ListCategory() {
   const [passid, setPassid] = useState('');
 
   const [category, setCategory] = useState([]);
+
+
+  const naviagte = useNavigate()
+
 
 
   const columns = [
@@ -30,19 +52,6 @@ function ListCategory() {
             setPassid(cellvalues.row.id);
            }}>
             <CategoryPopup id={passid}/>
-          </button>
-        )
-      }
-      },
-      {field: 'reject', headerName: 'Reject', width: 250 , 
-      renderCell: (cellvalues) => {
-        return (
-          <button
-          className='btn-pending'
-           onClick={(e)=>{
-            console.log(cellvalues)
-           }}>
-            Remove
           </button>
         )
       }
@@ -68,6 +77,7 @@ function ListCategory() {
 
   return (
     <div className='pending-table'>
+
         <div style={{ height: 400, width: '100%' }}>
             <DataGrid
                 rows={rowData}
