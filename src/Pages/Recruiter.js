0041e0a1e048e3593d85d672d1bb79257c493536
Amarnath <motorolaonefusion@gmail.com>
2022-12-10 22:53:35 +0530

    Updation in payment UI

--- src/Pages/Recruiter.js
+++ src/Pages/Recruiter.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import ReNavbar from '../Components/ReNavbar';
 import axios from '../axios';
 import {PayPalButtons} from '@paypal/react-paypal-js';
+import Paper from '@mui/material/Paper';
+
 
 
 function Recruiter() {
@@ -56,57 +58,119 @@ function Recruiter() {
       {profile.is_acceped ?  <ReNavbar/> :''}
       
       <div className='posted' >
-        {profile.is_requested ? <p style={{color: '#000' }}>'You request is pending'</p> : profile.is_acceped ? <div>
-        <p style={{color: '#000'}}>'You can add Job Now  <Link style={{color: '#000'}}  to='/post-job'>Here</Link>
-        </p>
-          
-        <PayPalButtons style={{color:"blue" , shape:"pill" , layout:"horizontal" , tagline:'false'}}
-            createOrder={(data, actions)=>{
-              return actions.order.create({
-                purchase_units:[
-                  {
-                    description : product.description,
-                    amount:{
-                      value: product.price
-                    },
+        {profile.is_requested ? <p style={{color: '#000' }}>'You request is pending'</p> :  profile.paid ? 
 
-                  },
-                ],
-              });
-            }}
-            onApprove = {async (data,actions)=>{
-                const order = await actions.order.capture();
-                console.log("order", order);
+          <div className='subscription-plan'>
 
-                handlePayment(order)
-                handleApprove(data.orderID)
+              <h2>You want to Purchance A plan to access All Features</h2>
 
-            }}
+              <div className="single-plans-card">
+              <Paper elevation={8}  id="inner-single-plan">
 
-            onCancel={() => {}}
+                  <h5>Payment Types</h5>
 
-            onError={(err)=>{
-              console.log('papal on error',err);
-            }}
+              <PayPalButtons style={{color:"blue" , shape:"pill" , layout:"horizontal" , tagline:'false'}}
+                  createOrder={(data, actions)=>{
+                    return actions.order.create({
+                      purchase_units:[
+                        {
+                          description : product.description,
+                          amount:{
+                            value: product.price
+                          },
 
-            onClick={(data, actions)=>{
-              const hasAlreadyBoughtCourse = false
+                        },
+                      ],
+                    });
+                  }}
+                  onApprove = {async (data,actions)=>{
+                      const order = await actions.order.capture();
+                      console.log("order", order);
 
-              if (hasAlreadyBoughtCourse) {
-                console.log('You already Bought this');
-                return actions.reject()
-              }
-              else{
-                return actions.resolve()
-              }
-            }}
+                      handlePayment(order)
+                      handleApprove(data.orderID)
 
-        />
+                  }}
 
-        </div> : 
-          <div>
-          <h1 style={{color: '#000'}}>If your Company already listed complete the Application<Link to='/application'>Here</Link> </h1>
-          <h1 style={{color: '#000'}}>If your Company not listed Add your Company<Link to='/add-company'>Here</Link> </h1>
+                  onCancel={() => {}}
+
+                  onError={(err)=>{
+                    console.log('papal on error',err);
+                  }}
+
+                  onClick={(data, actions)=>{
+                    const hasAlreadyBoughtCourse = false
+
+                    if (hasAlreadyBoughtCourse) {
+                      console.log('You already Bought this');
+                      return actions.reject()
+                    }
+                    else{
+                      return actions.resolve()
+                    }
+                  }}
+
+              />
+
+              </Paper> 
+
+
+
+              <Paper elevation={8}  id="inner-single-plan">
+
+                  <h5>Payment Types</h5>
+
+              <PayPalButtons style={{color:"blue" , shape:"pill" , layout:"horizontal" , tagline:'false'}}
+                  createOrder={(data, actions)=>{
+                    return actions.order.create({
+                      purchase_units:[
+                        {
+                          description : product.description,
+                          amount:{
+                            value: product.price
+                          },
+
+                        },
+                      ],
+                    });
+                  }}
+                  onApprove = {async (data,actions)=>{
+                      const order = await actions.order.capture();
+                      console.log("order", order);
+
+                      handlePayment(order)
+                      handleApprove(data.orderID)
+
+                  }}
+
+                  onCancel={() => {}}
+
+                  onError={(err)=>{
+                    console.log('papal on error',err);
+                  }}
+
+                  onClick={(data, actions)=>{
+                    const hasAlreadyBoughtCourse = false
+
+                    if (hasAlreadyBoughtCourse) {
+                      console.log('You already Bought this');
+                      return actions.reject()
+                    }
+                    else{
+                      return actions.resolve()
+                    }
+                  }}
+
+              />
+
+              </Paper> 
+              </div> 
+        </div> 
+        : profile.is_acceped ? 
+          <p style={{color: '#000'}}>'You can add Job Now  <Link style={{color: '#000'}}  to='/post-job'>Here</Link></p> 
+          :<div>
+            <h1 style={{color: '#000'}}>If your Company already listed complete the Application<Link to='/application'>Here</Link> </h1>
+            <h1 style={{color: '#000'}}>If your Company not listed Add your Company<Link to='/add-company'>Here</Link> </h1>
           </div>
         }
       </div>
